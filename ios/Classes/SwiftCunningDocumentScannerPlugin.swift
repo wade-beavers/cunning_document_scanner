0f87db65ed83bdbd904cf7de7aa6862b0f6f9adf
Wade Beavers <wade@blackairplane.com>
2023-04-12 14:27:28 -0400

    wip

--- ios/Classes/SwiftCunningDocumentScannerPlugin.swift
+++ ios/Classes/SwiftCunningDocumentScannerPlugin.swift
@@ -1,32 +1,29 @@
 import Flutter
 import UIKit
-import Vision
-import VisionKit
+import WeScan
 
-@available(iOS 13.0, *)
-public class SwiftCunningDocumentScannerPlugin: NSObject, FlutterPlugin, VNDocumentCameraViewControllerDelegate {
-   var resultChannel :FlutterResult?
-   var presentingController: VNDocumentCameraViewController?
+public class SwiftCunningDocumentScannerPlugin: NSObject, FlutterPlugin, ImageScannerControllerDelegate {
+    var resultChannel: FlutterResult?
+    var presentingController: ImageScannerController?
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "cunning_document_scanner", binaryMessenger: registrar.messenger())
-    let instance = SwiftCunningDocumentScannerPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "cunning_document_scanner", binaryMessenger: registrar.messenger())
+        let instance = SwiftCunningDocumentScannerPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if call.method == "getPictures" {
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "getPictures" {
             let presentedVC: UIViewController? = UIApplication.shared.keyWindow?.rootViewController
             self.resultChannel = result
-            self.presentingController = VNDocumentCameraViewController()
-            self.presentingController!.delegate = self
+            self.presentingController = ImageScannerController()
+            self.presentingController?.imageScannerDelegate = self
             presentedVC?.present(self.presentingController!, animated: true)
         } else {
             result(FlutterMethodNotImplemented)
             return
         }
-  }
-
+    }
 
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
@@ -34,30 +31,33 @@ public class SwiftCunningDocumentScannerPlugin: NSObject, FlutterPlugin, VNDocum
         return documentsDirectory
     }
 
-    public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
-        let tempDirPath = self.getDocumentsDirectory()
-        let currentDateTime = Date()
-        let df = DateFormatter()
-        df.dateFormat = "yyyyMMdd-HHmmss"
-        let formattedDate = df.string(from: currentDateTime)
-        var filenames: [String] = []
-        for i in 0 ... scan.pageCount - 1 {
-            let page = scan.imageOfPage(at: i)
-            let url = tempDirPath.appendingPathComponent(formattedDate + "-\(i).png")
-            try? page.pngData()?.write(to: url)
-            filenames.append(url.path)
-        }
-        resultChannel?(filenames)
-        presentingController?.dismiss(animated: true)
+   public func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
+    let tempDirPath = self.getDocumentsDirectory()
+    let currentDateTime = Date()
+    let df = DateFormatter()
+    df.dateFormat = "yyyyMMdd-HHmmss"
+    let formattedDate = df.string(from: currentDateTime)
+
+    guard let imageData = results.croppedScan.image.jpegData(compressionQuality: 1.0) else {
+        resultChannel?(nil)
+        scanner.dismiss(animated: true)
+        return
     }
 
-    public func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+    let url = tempDirPath.appendingPathComponent("\(formattedDate).jpg")
+    try? imageData.write(to: url)
+    resultChannel?([url.path])
+    scanner.dismiss(animated: true)
+}
+
+
+    public func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
         resultChannel?(nil)
-        presentingController?.dismiss(animated: true)
+        scanner.dismiss(animated: true)
     }
 
-    public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+    public func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
         resultChannel?(nil)
-        presentingController?.dismiss(animated: true)
+        scanner.dismiss(animated: true)
     }
 }
